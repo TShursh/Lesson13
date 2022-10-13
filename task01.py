25cc9767c4efae5a14f568e694d046e074a77009
Bazylevich <victoria.bazylevich@gmail.com>
2022-10-13 21:06:49 +0300

    1

--- task01.py
+++ task01.py
@@ -2,3 +2,6 @@ import timeit
 
 print("list: ", timeit.timeit("[1,2,3,4,5,6,7,8,9,10]", number=1_000_000_000))
 print("tuple: ", timeit.timeit("(1,2,3,4,5,6,7,8,9,10)", number=1_000_000_000))
+
+# git config --global user.name TShursh
+# git config --global user.email tati.irtik@gmail.com
